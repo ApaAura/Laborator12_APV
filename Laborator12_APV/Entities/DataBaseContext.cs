6202b6e96c7628561958116c76dfcce909e84e87
Apareci Aura <aurica.apareci@gmail.com>
2024-04-01 00:09:55 +0300

    DataBase Connection

---
+++ Laborator12_APV/Entities/DataBaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator12_APV.Entities
+{
+    public class DataBaseContext :DbContext
+    {
+        private readonly string? _connectionString;
+        public DataBaseContext()
+        {
+            Configurator configurator = new Configurator("appsettings.json");
+            _connectionString = configurator.ConnectionString;
+        }
+        public DataBaseContext(string? connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentNullException(nameof(connectionString));
+
+            _connectionString = connectionString;
+        }
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder opBuilder)
+        {
+            opBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
