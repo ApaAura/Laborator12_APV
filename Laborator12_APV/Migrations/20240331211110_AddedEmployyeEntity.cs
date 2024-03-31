ffd0eac114b1ff403350d2a2be8e16bc2d1cf76c
Apareci Aura <aurica.apareci@gmail.com>
2024-04-01 00:12:04 +0300

    Migration (run in Package Manager Console add-migration AddedEmployyeEntity)

---
+++ Laborator12_APV/Migrations/20240331211110_AddedEmployyeEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Laborator12_APV.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEmployyeEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EMPLOYEES",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    salary = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EMPLOYEES", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EMPLOYEES");
+        }
+    }
+}
